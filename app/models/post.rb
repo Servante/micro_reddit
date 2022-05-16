cca129c0344a238d212212feef643c42e01f056f
Wesley <matthew282@msn.com>
2022-05-16 12:25:35 -0700

    fixed associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-    validates :userid, presence: true, uniqueness: true
-    validates :title, presence: true, uniqueness: true, length: { minimum: 4}
+    validates :userid, presence: true
+    validates :title, presence: true, length: { minimum: 4}
     validates :post, presence: true
 
     belongs_to :user
+    has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
+    validates :username, presence: true, length: { minimum: 4 }
     validates :email, presence: true, uniqueness: true
 
     has_many :posts
+    has_many :comments
 end
